ba745b1b446f5751a9f9bf6cf2f38327c3bec6d7
Conrado Costa <conradocostaad@yahoo.com.br>
2022-09-05 23:53:04 -0300

    Starts implementing navigation and create dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HeroesComponent } from './heroes/heroes.component';
-import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  // {path: '/', component: AppComponent},
-  // {path: '/heroes', component: HeroesComponent}
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
