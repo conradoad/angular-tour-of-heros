ed07374621b90b8e163e096a57f1737c4c9a19d9
Conrado Costa <conradocostaad@yahoo.com.br>
2022-10-08 19:56:06 -0300

    Handling errors on getting heroes. Next: Update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
@@ -22,15 +23,31 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
+
   getHeroes(): Observable<Hero[]> {
-    const heroes = this.httpClient.get<Hero[]>(this.heroesUrl);
+    const heroes = this.httpClient.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log(`fetched heroes`)),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
     this.log('fetched heroes');
     return heroes;
   }
 
-  getHero(id: number): Observable<Hero|undefined> {
-    const hero = of(HEROES.find(hero => hero.id === id));
-    this.log('fetched hero id=${id}');
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    const hero = this.httpClient.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
     return hero;
   }
 }
